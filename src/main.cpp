a01b207224bb7758e6b89ddd6a7385af330434b5
Marvin Smith <marvin.smith@terminus-geo.com>
2025-08-17 10:20:41 -0600

    temp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
-/**
- * PicoCalc LVGL Graphics Demo
- *
- * Demo App Uses LVGL to show demos on the PicoCalc.
- * Implements basic keyboard input and display driver.
- *
- * Author: HsuahHanLai
- * https://www.hsuanhanlai.com/
- */
 
-#include <stdio.h>
-#include <string.h>
-#include "pico/stdlib.h"
+// C++ Standard Libraries
+#include <cstdio>
+#include <cstring>
+#include <filesystem>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// PICO APIs
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
-
+// LVGL
 #include <lv_conf.h>
 #include <lvgl/lvgl.h>
+
+// PicoCalc Drivers
 #include <picocalc/lv_port_indev_picocalc_kb.h>
 #include <picocalc/lv_port_disp_picocalc_ILI9488.h>
-#include <lvgl/demos/lv_demos.h>
 
+
+#include <resources/images/logo_235_65.hpp>
 
 #define ILI9488 1
 #define USE_DEFAULT_DEMO 1
@@ -27,7 +28,6 @@
 #define BYTE_PER_PIXEL (LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_RGB565)) /*will be 2 for RGB565 */
 
 const unsigned int LEDPIN = 25;
-
 
 // The event handler
 static void textarea_event_handler(lv_event_t *e) {
@@ -55,29 +55,51 @@ int main()
     lv_port_indev_init();
 
 
-
-#ifdef USE_DEFAULT_DEMO
-    // Initialize and run LVGL demo
-    lv_demo_widgets(); // Replace with the correct demo function if needed
-#else
     // Create a screen
     lv_obj_t *screen = lv_obj_create(NULL);
 
+    // Parent Column
+    lv_obj_t* cont_col = lv_obj_create(screen);
+    lv_obj_set_size(cont_col, 315, 315);
+    lv_obj_align(cont_col, LV_ALIGN_TOP_MID, 0, 5);
+    lv_obj_set_flex_flow(cont_col, LV_FLEX_FLOW_COLUMN);
+
     // The textarea
-    lv_obj_t *input = lv_textarea_create(screen);
+    lv_obj_t *input = lv_textarea_create(cont_col);
     lv_obj_set_width(input, 280);
-    lv_obj_set_height(input, LV_SIZE_CONTENT);    /// 20
+    lv_obj_set_height(input, LV_SIZE_CONTENT);
     lv_obj_center(input);
+
+    // Image
+    //LV_IMAGE_DECLARE( logo_235_65 );
+    //lv_obj_t * img1 = lv_image_create(cont_col);
+    //lv_image_set_src(img1, &logo_235_65);
+
+
     // Enable keyboard input for the text box
     lv_textarea_set_placeholder_text(input, "Input:");
     lv_textarea_set_one_line(input, true);
+
     lv_obj_set_style_anim_time(input, 5000, LV_PART_CURSOR|LV_STATE_FOCUSED);
+
     // Textarea event handler
     lv_obj_add_event_cb(input, textarea_event_handler, LV_EVENT_READY, input);
 
+    /// File Lister
+    lv_obj_t *file_lister = lv_textarea_create(cont_col);
+    lv_obj_set_size( file_lister, 300, 270 );
+    lv_obj_center(file_lister);
+
+    std::stringstream file_text;
+    file_text << "Content\n";
+    for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{"/"} ) {
+        file_text << dir_entry << std::endl;
+    }
+
+    lv_textarea_set_text( file_lister, file_text.str().c_str() );
+
     // Load the screen
     lv_scr_load(screen);
-#endif
 
     // Main loop
     while (1)
